15da4b5c6d95eae00c440aceb8f9d3e36cc6d67d
Aria Minaei <aria.minaei@gmail.com>
2023-02-05 13:35:18 +0100

    s

---
+++ t.mjs
@@ -0,0 +1,11 @@
+import {echo} from "zx"
+
+const data = {
+  a: 1,
+  b: "a string",
+  d: {
+    val: `a nested string, with }, and {, and quotes \", and backslashes \\, and newlines\n, and backticks\``,
+  },
+}
+
+await echo`{data}={${JSON.stringify(data)}}`
